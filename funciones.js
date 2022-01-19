51a27584f38643e31558d47f73496b29cc95bcac
Rubén <rmartinezdominguez@danielcastelao.org>
2022-01-19 17:19:39 +0100

    Commit inicial

---
+++ funciones.js
@@ -0,0 +1,18 @@
+/**
+ * Función para que el usuario decida si A o si B
+ * @returns cocheocamion - que viene siendo A o B
+ * 
+ */
+function decidirsicocheocamion() {
+    var decision = false
+    do {
+        var cocheocamion = prompt("Escribe 1 si quieres introducir un coche o 2 si quieres introducir un camión");
+        if (cocheocamion == 1) {
+            decision = true;
+            return cocheocamion;
+        } else if (cocheocamion == 2) {
+            decision = true;
+            return cocheocamion;
+        }
+    } while (!decision)
+}
